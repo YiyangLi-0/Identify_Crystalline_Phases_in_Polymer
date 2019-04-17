39ed22e897eb49122adb59e63b6367e267987614
saturas <liyiyang010@163.com>
2019-04-17 16:32:57 -0700

    Improved the robustness of the multiprocessing run mode.

--- src/modules/identify_mp.py
+++ src/modules/identify_mp.py
@@ -13,8 +13,9 @@ def possible_crys_atoms(mds, bins, inp):
     n_proc = multiprocessing.cpu_count()
     
     ''' Create and submit jobs. '''
-    jobs  = [None] * n_proc
-    pipes = [None] * n_proc
+    jobs  = [None for _ in range(n_proc)]
+    pipes = [None for _ in range(n_proc)]
+    master_pca = []
 
     # Partition atoms for parallel processing.
     chunked_atoms = md_system.distribute_atoms(range(mds.n_atom), n_proc)
@@ -22,29 +23,27 @@ def possible_crys_atoms(mds, bins, inp):
     # Initialize a list to store the ids of possible crystalline atoms
     # (pca) for every child process.
     L   = max([len(_) for _ in chunked_atoms])  # Length of pca.
-    pca = [-1] * L
+    pca = [-1 for _ in range(L)]
 
     for i in range(n_proc):
         loc_atoms = chunked_atoms[i]
 
         # Create and submit jobs.
-        parent, child = multiprocessing.Pipe(False)
+        parent, child = multiprocessing.Pipe(duplex=False)
         p = multiprocessing.Process(
                 name   = 'local-rank_{}'.format(i),
                 target =  parallel_job,
                 args   = (mds, bins, inp, loc_atoms, pca, child))
         p.start()
+        child.close()
         jobs[i]  = p
         pipes[i] = parent
 
-    ''' Wait for all child processes to complete. '''
-    for p in jobs:
-        p.join()
+    for i,parent in enumerate(pipes):
+        # SEQUENTIALLY wait for each child process to complete and collect data
+        master_pca.extend(parent.recv())
+        jobs[i].join()
 
-    ''' Collect pca from child processes to master process. '''
-    master_pca = []
-    for parent in pipes:
-        master_pca += parent.recv()     # Receive from the child end of pipe.
     master_pca = list(set(master_pca))  # Trim additional -1s.
     master_pca.remove(-1)
     return master_pca  # 0-indexed.
@@ -55,6 +54,7 @@ def parallel_job(mds, bins, inp, loc_atoms, pca, child):
     proc = multiprocessing.current_process().name
     p2_parameters.p2_parameters(mds, bins, inp, loc_atoms, pca, proc)
     child.send(pca)  # Send to the parent end of pipe.
+    child.close()
 
 
 if __name__ == '__main__':
